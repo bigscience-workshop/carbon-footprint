d81e559f9236e8287dccab720071f63f4b72ee05
Sylvain Viguier <wallacehobbes@gmail.com>
2022-01-29 22:49:30 +0000

    Add script to log info from SLURM jobs

---
+++ jobs_info.py
@@ -0,0 +1,110 @@
+#!/bin/python3
+"""Script used to print useful metrics from runs"""
+
+# Orginal script
+from doctest import FAIL_FAST
+import sys
+import subprocess
+import collections
+import re
+from enum import Enum, auto
+
+class GpuType(Enum):
+    V100_32GB = auto()
+    V100_16GB = auto()
+    A100_40GB = auto()
+
+class Node:
+    def __init__(self, gpu_type, num_gpus):
+        self.gpu_type = gpu_type
+        self.num_gpus = num_gpus
+
+type2partitions = [(Node(GpuType.V100_32GB, 4), 'gpu_p1'),
+                   (Node(GpuType.V100_32GB, 8), 'gpu_p2'),
+                   (Node(GpuType.V100_16GB, 4), 'gpu_p3'),
+                   (Node(GpuType.A100_40GB, 8), 'gpu_p4')]
+
+node2type = {}
+for node_type, partition in type2partitions:
+    p = subprocess.run(f'/gpfslocalsys/slurm/current/bin/sinfo -N -p {partition} --Format=nodehost -h',
+                       shell=True, encoding='utf8',
+                       stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    for node in p.stdout.splitlines():
+        node2type[node.strip()] = node_type
+
+
+def find_node_type(node):
+    try:
+        return node2type[node]
+    except:
+        return None
+
+
+def find_num_gpus_per_types(nodelist, alloctres):
+    p = subprocess.run(f'/gpfslocalsys/slurm/current/bin/scontrol show hostnames {nodelist}',
+                       shell=True, encoding='utf8',
+                       stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    nodes = p.stdout.splitlines()
+    is_single_node = (len(nodes) == 1)
+
+    if is_single_node:
+        m = re.search('gpu=(\d+)', alloctres)
+        num_gpus = int(m.group(1)) if m else 0
+
+    num_gpus_per_types = collections.defaultdict(int)
+    for node in nodes:
+        node_type = find_node_type(node)
+        if node_type:
+            num_gpus_per_types[node_type.gpu_type] += num_gpus if is_single_node else node_type.num_gpus
+    return num_gpus_per_types
+
+
+# Modifications to show more info for each job
+def split_alloctres(alloctres):
+    line_splitted = [i.split('=') for i in alloctres.split(',')]
+    d = {}
+    for pair in line_splitted:
+        key = pair[0]
+        value = pair[1]
+        d[key] = (value)
+    return d
+
+
+show_headers = not ('-n' in sys.argv[1:] or '--noheader' in sys.argv[1:])
+args = ['sacct'] + sys.argv[1:] + ['--format=jobid,elapsed,nodelist,alloctres,partition,qos,start,end,group', '-P', '-X', '-n']
+
+p = subprocess.run(' '.join(args), shell=True, encoding='utf8',
+                   stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+if show_headers:
+    # Print human-readable output
+    fmt_string = '{0:<9} {1:<9} {2:<9} {3:<9} {4:<9} {5:<9} {6:<9} {7:<9} {8:<9} {9:<9} {10:<10} {11:<19} {12:<19}'
+    print(fmt_string.format('JobID', 'V100 32GB', 'V100 16GB', 'A100 40GB', 'CPUs', 'RAM', 'Energy', 'Partition', 'Group', 'Elapsed', 'QoS', 'Start', 'End'))
+    print(('-' * 9 + ' ') * 10 + ('-' * 10 + ' ') + ('-' * 19 + ' ') * 2)
+
+else:
+    # Machine readable output
+    fmt_string = '{0}|{1}|{2}|{3}|{4}|{5}|{6}|{7}|{8}|{9}|{10}|{11}|{12}'
+
+for j in p.stdout.splitlines():
+    job_id, elapsed, nodelist, alloctres, partition, qos, start, end, group = j.split(
+        '|')
+
+    num_gpus_per_types = find_num_gpus_per_types(nodelist, alloctres)
+
+    alloc = split_alloctres(alloctres)
+
+    print(
+        fmt_string.format(job_id,
+                          num_gpus_per_types[GpuType.V100_32GB],
+                          num_gpus_per_types[GpuType.V100_16GB],
+                          num_gpus_per_types[GpuType.A100_40GB],
+                          alloc['cpu'],
+                          alloc['mem'], 
+                          alloc['energy'],
+                          partition,
+                          group,
+                          elapsed,
+                          qos,
+                          start,
+                          end))
